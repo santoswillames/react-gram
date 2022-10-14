256185439cce62c171bee4dd263cfe57c5882b4d
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-10-14 13:10:26 -0300

    Initial project and create server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+// Modulo do própio node para trabalhar com com diretório, vamos usar para salvar as imagens
+const path = require("path");
+const cors = require("cors");
+
+const port = process.env.PORT;
+
+const app = express();
+
+// config JSON and form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}, acesse http://localhost:${port}`);
+});
