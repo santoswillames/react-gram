2928c598d08f8433c3147cc58bb52410dbe9f36e
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-10-20 13:18:49 -0300

    Create delete photo function

--- server/controllers/PhotoController.js
+++ server/controllers/PhotoController.js
@@ -24,11 +24,49 @@ const insertPhoto = async (req, res) => {
     res.status(422).json({
       errors: ["Erro inesperado! Por favor tente mais tarde"],
     });
+    return;
   }
 
   res.status(201).json(newPhoto);
 };
 
+// Remove a photo from DB
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+    // Check if photo exists
+    if (!photo) {
+      res.status(404).json({ errors: ["Foto não encontrada!"] });
+      return;
+    }
+
+    // Check photo belongs to user
+    if (!photo.userId.equals(reqUser._id)) {
+      res.status(422).json({
+        errors: ["Ocorreu um erro, tente mais tarde."],
+      });
+      return;
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    // ID -> da photo podemos deletar ela de uma lista no front, e ai não precisamos de uma outra requisção no back
+    // MESSAGE -> Para exibir no front para o usuário
+    res
+      .status(200)
+      .json({ id: photo._id, message: "Foto excluida com sucesso" });
+  } catch (error) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
+};
+
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
