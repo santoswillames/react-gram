3401e6ff98839287fb5721c0806fbd6a133291d4
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-10-21 15:14:20 -0300

    controlled register page inputs

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -7,8 +7,20 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [formValues, setFormValues] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const user = Object.fromEntries(formData);
   };
 
   return (
@@ -16,10 +28,34 @@ const Register = () => {
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <input type="password" placeholder="Confirme sua senha" />
+        <input
+          type="text"
+          placeholder="Nome"
+          name="name"
+          onChange={handleInputChange}
+          value={formValues.name || ""}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+          onChange={handleInputChange}
+          value={formValues.email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          onChange={handleInputChange}
+          value={formValues.password || ""}
+        />
+        <input
+          type="password"
+          placeholder="Confirme sua senha"
+          name="confirmPassword"
+          onChange={handleInputChange}
+          value={formValues.confirmPassword || ""}
+        />
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
