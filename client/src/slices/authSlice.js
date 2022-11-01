eb5f103a0bac166a7dda8a3eaf62369ffbde2608
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-01 11:21:44 -0300

    create page login and authentication

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -28,6 +28,20 @@ export const register = createAsyncThunk(
   }
 );
 
+// Sign in an user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  // O user virá do componente que foi criado -> Register
+  const data = await authService.login(user);
+
+  // Check for errors
+  if (data.errors) {
+    // Rejeição da requisição
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
 // Logout an user
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
@@ -64,6 +78,21 @@ export const authSlice = createSlice({
         state.loading = false;
         state.success = true;
         state.error = null;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
         state.user = null;
       });
   },
