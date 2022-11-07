54f5fd0d51a151f6bd167eb195e9bc0372265b61
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-07 10:31:49 -0300

    add functions in EditProfile page

--- client/src/pages/EditProfile/EditProfile.jsx
+++ client/src/pages/EditProfile/EditProfile.jsx
@@ -1,8 +1,56 @@
 import "./EditProfile.css";
 
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+// Components
+import Message from "../../components/Message";
+
 const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  // States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Fill form with user data
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
+    }
+  }, [user]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleFile = (e) => {
+    // image preview
+    const image = e.target.files[0];
+
+    setPreviewImage(image);
+
+    // update image state
+    setProfileImage(image);
   };
 
   return (
@@ -11,21 +59,46 @@ const EditProfile = () => {
       <p className="subtitle">
         Adicione uma imagem de perfil e conte mais sobre você...
       </p>
-      {/* Preview da imagem */}
+      {(user.profileImage || previewImage) && (
+        <img
+          className="profile-image"
+          src={
+            previewImage
+              ? URL.createObjectURL(previewImage)
+              : `${uploads}/users/${user.profileImage}`
+          }
+          alt={user.name}
+        />
+      )}
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="E-mail" disabled value={email || ""} />
         <label>
           <span>Imagem do perfil</span>
-          <input type="file" />
+          <input type="file" onChange={handleFile} />
         </label>
         <label>
           <span>Bio:</span>
-          <input type="text" placeholder="Descrição do perfil." />
+          <input
+            type="text"
+            placeholder="Descrição do perfil."
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ""}
+          />
         </label>
         <label>
           <span>Quer alterar sua senha?</span>
-          <input type="password" placeholder="Digite sua nova senha" />
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
         </label>
         <input type="submit" value="Atualizar" />
       </form>
