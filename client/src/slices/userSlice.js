03ddd5df111c69c19187367db2f516f7f9fd1752
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-09 11:00:44 -0300

    create service and slice of page profile

--- client/src/slices/userSlice.js
+++ client/src/slices/userSlice.js
@@ -38,6 +38,13 @@ export const updateProfile = createAsyncThunk(
   }
 );
 
+//  Get user Details
+export const getUserDetails = createAsyncThunk("user/get", async (id) => {
+  const data = await userService.getUserDetails(id);
+
+  return data;
+});
+
 export const userSlice = createSlice({
   name: "user",
   initialState,
@@ -73,6 +80,16 @@ export const userSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
         state.user = {};
+      })
+      .addCase(getUserDetails.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getUserDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
       });
   },
 });
