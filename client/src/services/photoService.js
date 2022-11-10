1dab17c16a70b7fd3b05579d9aab8227f215f743
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-10 11:15:27 -0300

    create function get photo slice and service

--- client/src/services/photoService.js
+++ client/src/services/photoService.js
@@ -14,8 +14,23 @@ const publishPhoto = async (data, token) => {
   }
 };
 
+// Get an user photos
+const getUserPhotos = async (id, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
+  getUserPhotos,
 };
 
 export default photoService;
