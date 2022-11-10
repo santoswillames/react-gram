89ad17a95c19e6814f56a4cf9efb1e5b3c76bdbb
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-10 10:40:28 -0300

    create publish photo slice and service

--- client/src/services/photoService.js
+++ client/src/services/photoService.js
@@ -1,5 +1,21 @@
 import { api, requestConfig } from "../utils/config";
 
-const photoService = {};
+// Publish an user photo
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(api + "/photo", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = {
+  publishPhoto,
+};
 
 export default photoService;
