3208eca1055ecaf9981c82081bb3323f7da7617f
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-14 10:57:46 -0300

    create slice and service of details photo page

--- client/src/slices/photoSlice.js
+++ client/src/slices/photoSlice.js
@@ -77,6 +77,13 @@ export const updatePhoto = createAsyncThunk(
   }
 );
 
+// Get Photo by Id
+export const getPhoto = createAsyncThunk("photo/getphoto", async (id) => {
+  const data = await photoService.getPhoto(id);
+
+  return data;
+});
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -152,6 +159,16 @@ export const photoSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
         state.photo = {};
+      })
+      .addCase(getPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
       });
   },
 });
