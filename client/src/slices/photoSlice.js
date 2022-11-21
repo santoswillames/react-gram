0b6dede00d64ee0e532389bf7bc1298bcf31eb60
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-21 10:18:12 -0300

    page home finished

--- client/src/slices/photoSlice.js
+++ client/src/slices/photoSlice.js
@@ -125,11 +125,16 @@ export const comment = createAsyncThunk(
 );
 
 // Get all photos
-export const getPhotos = createAsyncThunk("photo/getall", async () => {
-  const data = await photoService.getPhotos();
+export const getPhotos = createAsyncThunk(
+  "photo/getall",
+  async (_, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
 
-  return data;
-});
+    const data = await photoService.getPhotos(token);
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
