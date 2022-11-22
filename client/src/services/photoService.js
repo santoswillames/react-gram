95cd172922c8743383308fcbebe9fa5d57b334f0
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-22 09:31:01 -0300

    create search slice and service

--- client/src/services/photoService.js
+++ client/src/services/photoService.js
@@ -113,6 +113,20 @@ const getPhotos = async (token) => {
   }
 };
 
+// Search photo by title
+const searchPhotos = async (query, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/search?q=" + query, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -122,6 +136,7 @@ const photoService = {
   like,
   comment,
   getPhotos,
+  searchPhotos,
 };
 
 export default photoService;
