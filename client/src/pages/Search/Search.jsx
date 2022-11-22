30940d23ae1708b7056a85e0b4164ddda056d24a
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-11-22 10:04:31 -0300

    project finished

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -7,14 +7,60 @@ import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 import { useQuery } from "../../hooks/useQuery";
 
 // Components
-import LinkContainer from "../../components/LikeContainer";
+import LikeContainer from "../../components/LikeContainer";
 import PhotoItem from "../../components/PhotoItem";
 import { Link } from "react-router-dom";
 
 // Redux
+import { searchPhotos, like } from "../../slices/photoSlice";
 
 const Search = () => {
-  return <div>Search</div>;
+  const query = useQuery();
+  const search = query.get("q");
+
+  const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
+
+  const { user } = useSelector((state) => state.auth);
+  const { photos, loading } = useSelector((state) => state.photo);
+
+  // Load photos
+  useEffect(() => {
+    dispatch(searchPhotos(search));
+  }, [dispatch, search]);
+
+  // Like a photo
+  const handleLike = (photo) => {
+    dispatch(like(photo._id));
+
+    resetMessage();
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="search">
+      <h2>Você está buscando por: {search}</h2>
+      {photos &&
+        photos.map((photo) => (
+          <div key={photo._id}>
+            <PhotoItem photo={photo} />
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <Link className="btn" to={`/photos/${photo._id}`}>
+              Ver mais
+            </Link>
+          </div>
+        ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Não foram encontrado fotos para a sua busca...
+        </h2>
+      )}
+    </div>
+  );
 };
 
 export default Search;
